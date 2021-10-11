41f96305d72c9e4561b9454752ca1599c187b5bc
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2021-10-12 02:49:38 +0530

    Auto format on code push (#2)
    * ci(ci): :construction_worker: Change ci to auto format code
    
    * Automated autoyapf fixes
    
    * ci(ci): :construction_worker: Change yapf style to use setup.cfg
    
    * Automated autoyapf fixes
    
    * ci(github): :construction_worker: Changed CI message for automatic formatting
    
    * ci(yapf): :construction_worker: Automated yapf fixes

--- APIs/RedditScraper.py
+++ APIs/RedditScraper.py
@@ -9,105 +9,104 @@ from alive_progress import alive_bar
 
 ## define the class
 
+
 class RedditScraper(object):
+  '''
+    
     
     '''
-    
-    
-    '''
-    
-    
-    def __init__(self, subred, queries):
-        
-        self.subr = subred
-        
-        self.subq = queries
-        
-        self.reddit_api = PushshiftAPI()
-        
-        
-    def extract_threads(self):
-    
-       ls = []
 
-       with alive_bar(100) as bar:
-                
-            for channel in self.subr:
+  def __init__(self, subred, queries):
 
-                for query in self.subq:
+    self.subr = subred
 
-                    generator_obj_1 = self.reddit_api.search_comments(q=query, subreddit=channel)
+    self.subq = queries
 
-                    submissions = pd.DataFrame([comment.d_ for comment in generator_obj_1])
+    self.reddit_api = PushshiftAPI()
 
-                    submissions['Search_Term'] = query
+  def extract_threads(self):
 
-                    submissions['Search_Subreddit'] = channel
+    ls = []
 
-                    ls.append(submissions)
-                    
-                    bar()
+    with alive_bar(100) as bar:
 
-                    yield print('Comment extraction complete for {} in {}'.format(query,channel)) ## find a way to log to user
+      for channel in self.subr:
 
+        for query in self.subq:
 
-            df = pd.concat(ls)
-            
-            csv_out = df.to_csv('RedditComments-8-24.csv')
-            
-            json_out = df.to_json(orient='records')
+          generator_obj_1 = self.reddit_api.search_comments(q=query,
+                                                            subreddit=channel)
 
-            return csv_out, json_out
+          submissions = pd.DataFrame(
+              [comment.d_ for comment in generator_obj_1])
+
+          submissions['Search_Term'] = query
+
+          submissions['Search_Subreddit'] = channel
+
+          ls.append(submissions)
+
+          bar()
+
+          yield print('Comment extraction complete for {} in {}'.format(
+              query, channel))  ## find a way to log to user
+
+      df = pd.concat(ls)
+
+      csv_out = df.to_csv('RedditComments-8-24.csv')
+
+      json_out = df.to_json(orient='records')
+
+      return csv_out, json_out
 
 ##############
-       
-    def extract_comments(self):
-       
-       
-       ls = []
-       
-       with alive_bar(100) as bar:
-           
-            for channel in self.subr:
 
-                for query in self.subq:
+  def extract_comments(self):
 
-                    generator_obj = self.reddit_api.search_submissions(q=query, subreddit=channel)
+    ls = []
 
-                    submissions = pd.DataFrame([submission.d_ for submission in generator_obj])
+    with alive_bar(100) as bar:
 
-                    submissions['Search_Term'] = query
+      for channel in self.subr:
 
-                    submissions['Search_Subreddit'] = channel
+        for query in self.subq:
 
-                    ls.append(submissions)
-                    
-                    bar()
+          generator_obj = self.reddit_api.search_submissions(q=query,
+                                                             subreddit=channel)
 
-                    yield print('Thread extraction complete for {} in {}'.format(query,channel))
+          submissions = pd.DataFrame(
+              [submission.d_ for submission in generator_obj])
+
+          submissions['Search_Term'] = query
+
+          submissions['Search_Subreddit'] = channel
+
+          ls.append(submissions)
+
+          bar()
+
+          yield print('Thread extraction complete for {} in {}'.format(
+              query, channel))
+
+      df = pd.concat(ls)
+
+      csv_out = df.to_csv('RedditThreads-8-24.csv')
+
+      json_out = df.to_json(orient='records')
+
+      return csv_out, json_out
+
+  def run_scraper(self):
+
+    dl_csv_threads, dl_json_threads = self.extract_threads()
+
+    dl_csv_comms, dl_json_comms = self.extract_comments()
+
+    return dl_csv_comms, dl_json_threads, dl_json_comms, dl_csv_threads
 
 
-            df = pd.concat(ls)
-            
-            csv_out = df.to_csv('RedditThreads-8-24.csv')
-            
-            json_out = df.to_json(orient='records')
-            
-        
-            return csv_out, json_out
-   
-   
-    def run_scraper(self):
-        
-        dl_csv_threads, dl_json_threads = self.extract_threads()
-        
-        dl_csv_comms, dl_json_comms = self.extract_comments()
-        
-        return dl_csv_comms, dl_json_threads, dl_json_comms, dl_csv_threads
-        
-       
-###########       
+###########
 
 if __name__ == "__main__":
 
-    pass
+  pass
--- NLP/Classification.py
+++ NLP/Classification.py
@@ -1,7 +1,7 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 import re
-import os 
+import os
 import random
 import spacy
 from spacy.util import minibatch, compounding
@@ -12,61 +12,51 @@ from sklearn.metrics import accuracy_score
 import tensorflow as tf
 
 
-
 class BinaryClassifier(object):
-    
-    def __init__(self,articles,labels):
-        
-        
-        ## data bundling from raw data ## split T/T data
-        
-        ## base model 
-    
-        self.cv = CountVectorizer()
-        
-        self.model = LogisticRegression()
-        
-        ## optimiser 
-        
-  
+
+  def __init__(self, articles, labels):
+
+    ## data bundling from raw data ## split T/T data
+
+    ## base model
+
+    self.cv = CountVectorizer()
+
+    self.model = LogisticRegression()
+
+    ## optimiser
+
+
 class BertClassifier(object):
-    
-    def __init__(self):
-    
-        self.model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
-        
-        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-        
-    
+
+  def __init__(self):
+
+    self.model = TFBertForSequenceClassification.from_pretrained(
+        "bert-base-uncased")
+
+    self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+
 class GlobalOptimiser(object):
-    
-    def __init__(self, lrate, eps, cnorm):
-        
-        self.optimiser = tf.keras.optimizers.Adam(
-            learning_rate=lrate, epsilon=eps,
-            clipnorm=cnorm
-        )
 
-        ## loss function
+  def __init__(self, lrate, eps, cnorm):
 
-        loss = tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits=True)
+    self.optimiser = tf.keras.optimizers.Adam(learning_rate=lrate,
+                                              epsilon=eps,
+                                              clipnorm=cnorm)
 
-        ## metrics
+    ## loss function
 
-        metrics = [tf.keras.metrics.SparseCategoricalAccuracy('accuracy')]
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-        ## compile the optimiser
+    ## metrics
 
-        model.compile(optimizer=self.optimizer,
-                           loss=self.loss, metrics=self.metrics)
+    metrics = [tf.keras.metrics.SparseCategoricalAccuracy('accuracy')]
 
-        ## fit the model
-        
-        
-    
-    
-    
+    ## compile the optimiser
 
-    
-    
+    model.compile(optimizer=self.optimizer,
+                  loss=self.loss,
+                  metrics=self.metrics)
+
+    ## fit the model
