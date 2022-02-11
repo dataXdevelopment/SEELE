4c864b5d3b77c618e5265975a380370415835403
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-02-12 01:52:23 +0530

    feat(scrapers): Scraper loads environment variables

--- workers/meta_critic.py
+++ workers/meta_critic.py
@@ -63,7 +63,10 @@ def updateStatus(id, status, result=None):
     print(result)
 
 
-config = dotenv_values(".env")
+config = {
+    **dotenv_values(".env"),  # load shared development variables
+    **os.environ,  # override loaded values with environment variables
+}
 
 transport = AIOHTTPTransport(url=config["DAGON_GRAPHQL_URL"],
                              headers={'x-api-key': config["WORKER_API_KEY"]})
